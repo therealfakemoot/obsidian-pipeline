dd222fe847d7ceea15fa9d3079d83b2dccd36620
Nick Dumas <nobodys@business.com>
2023-05-03 09:44:34 -0400

    whatever

---
+++ cmd/obp/cmd/validate.go
@@ -0,0 +1,29 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var validateCmd = &cobra.Command{
+	Use:   "validate",
+	Short: "loads a note and ensures its frontmatter follows the provided protobuf schema",
+	Long: `parses frontmatter in a markdown note and attempts to write the struct out using the provided protobuf schema.
+
+If the write succeeds, the data satisfies the schema.
+`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+func init() {
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	validateCmd.Flags().StringP("schema", "s", "schema.proto", "path to protobuf file")
+	rootCmd.AddCommand(validateCmd)
+}
---
+++ cmd/obp/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "code.ndumas.com/ndumas/obsidian-pipeline/cmd/obp/cmd"
+
+func main() {
+	cmd.Execute()
+}
